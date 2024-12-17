88d2af41a6244d2ebd6fffe3adedd0ef0ceb6135
Laszlo Kiraly <laszlo.kiraly@est.tech>
2024-12-17 10:54:50 +0100

    scope reduced, delete unused directories
    Signed-off-by: Laszlo Kiraly <laszlo.kiraly@est.tech>

--- performance_testing/scripts/nsm_clear_dns.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-kubectl "--kubeconfig=$KUBECONFIG1" delete service -n kube-system exposed-kube-dns
-kubectl "--kubeconfig=$KUBECONFIG2" delete service -n kube-system exposed-kube-dns
-
-true
